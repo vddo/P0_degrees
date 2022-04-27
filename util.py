a3720faa8af6604541d04ede7735bc8f6db5298b
ducAI <duc.do32@mailbox.org>
2022-04-27 16:36:27 +0200

    util v0.01 class Node()

--- util.py
+++ util.py
@@ -1,8 +1,15 @@
 class Node():
-    def __init__(self, state, parent, action):
-        self.state = state
-        self.parent = parent
-        self.action = action
+    def __init__(self, person_id, con_movie_id):
+        # from original util.py
+        # self.state = state
+        # self.parent = parent
+        # self.action = action
+        self.person_id = person_id  # new node preferences
+        self.con_movie_id = con_movie_id
+        # self.movie_ids = movie_ids
+        # self.order = order+1
+        # self.neighbors = neighbors
+        # self.persons_on_path = persons_on_path
 
 
 class StackFrontier():
@@ -12,8 +19,11 @@ class StackFrontier():
     def add(self, node):
         self.frontier.append(node)
 
-    def contains_state(self, state):
-        return any(node.state == state for node in self.frontier)
+# To be tested#
+    def contains_node(self, person_id, movie_id):
+        for node in self.frontier:
+            if any(person_id == node.person_id and movie_id == node.movie_id):
+                return
 
     def empty(self):
         return len(self.frontier) == 0
